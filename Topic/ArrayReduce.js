3994d8ce4c009165d5ba1a0a2e6b655b32beaff5
Shahin <shahinu886@gmail.com>
2025-10-21 12:34:37 +0530

    update

---
+++ Topic/ArrayReduce.js
@@ -0,0 +1,20 @@
+// Array Reduce are used to execute a reducer function on each element of the array, resulting in a single output value.
+// The reduce method processes each element of the array (from left to right) and accumulates a single result based on the logic defined in the reducer function.
+
+// Here is an example of how to use the reduce method in JavaScript:
+// Creating an array of numbers
+let numbers = [1, 2, 3, 4, 5];
+
+// Using reduce to calculate the sum of all numbers in the array
+let sum = numbers.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+}, 0);
+console.log(sum);
+// Output: 15
+
+// Using reduce to find the maximum value in the array
+let max = numbers.reduce(function(accumulator, currentValue) {
+    return Math.max(accumulator, currentValue);
+}, numbers[0]);
+console.log(max);
+// Output: 5
