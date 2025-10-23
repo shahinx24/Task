39ccab21efabc2dc9988597d7836531ac532ccaa
Shahin <shahinu886@gmail.com>
2025-10-23 19:55:25 +0530

    update2

---
+++ Topic/arrayforeach.js
@@ -0,0 +1,17 @@
+// Array For Each is used to execute a provided function once for each array element.
+// The forEach method does not return a new array; it simply executes the function for each element in the original array.
+
+// Here is an example of how to use the forEach method in JavaScript:
+
+// Creating an array of numbers
+let numbers = [1, 2, 3, 4, 5];
+// Using forEach to log each number to the console
+numbers.forEach(function(num) {
+    console.log(num);
+});
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
---
+++ Topic/declaration.js
@@ -0,0 +1,21 @@
+// Array Declaration is used to create a new array in JavaScript.
+// There are several ways to declare an array, including using array literals and the Array constructor.
+
+// Here is an example of how to declare arrays in JavaScript:
+// Declaring an array using array literal syntax
+let fruits = ['Apple', 'Banana', 'Cherry'];
+console.log(fruits);
+// Output: ['Apple', 'Banana', 'Cherry']
+
+// Declaring an array using the Array constructor
+let numbers = new Array(1, 2, 3, 4, 5);
+console.log(numbers);
+// Output: [1, 2, 3, 4, 5]
+
+// Declaring an empty array and adding elements later
+let colors = [];
+colors.push('Red');
+colors.push('Green');
+colors.push('Blue');
+console.log(colors);
+// Output: ['Red', 'Green', 'Blue']
