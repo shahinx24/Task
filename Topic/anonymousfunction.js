9f63e3ad38509d953b7772570cd03cb3afdc2daa
Shahin <shahinu886@gmail.com>
2025-10-30 10:47:22 +0530

    mes

--- Topic/anonymousfunction.js
+++ Topic/anonymousfunction.js
@@ -17,7 +17,7 @@ setTimeout(function() {
 let numbers = [1, 2, 3, 4, 5];
 let squares = numbers.map(function(num) {
     return num * num;
-});
+}); 
 console.log(squares); // Output: [1, 4, 9, 16, 25]
 
 // Using an anonymous function with event listeners
