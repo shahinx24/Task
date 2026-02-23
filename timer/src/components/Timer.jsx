98252daf928f1cc10867b288fa66cd7acebd18bd
Shahin <shahinofficial911@gmail.com>
2026-02-23 10:46:56 +0530

    Update

---
+++ timer/src/components/Timer.jsx
@@ -0,0 +1,27 @@
+import { useState,useEffect } from "react";
+
+export default function Timer(){
+    const [seconds, setSeconds] = useState(0)
+    const [running, setRunning] = useState(false)
+
+    useEffect(()=>{
+        if(!running) return;
+
+        const interval = setInterval(()=>{
+            setSeconds((prev)=> prev + 1);
+        },1000)
+        return ()=> clearInterval(interval);
+    },[running]);
+
+    const FormatTime = ()=>{
+        const m = Math.floor(seconds/ 60);
+        const s = seconds % 60;
+        return `${String(m).padStart(2,"0")}:${String(s).padStart(2,"0")}`;
+    }
+
+    return(
+        <>
+        <h2></h2>
+        </>
+    )
+}
