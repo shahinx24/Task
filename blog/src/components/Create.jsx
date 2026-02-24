8848badb816789c5fbd12d2fcf5c938e1c44b700
Shahin <shahinofficial911@gmail.com>
2026-02-24 11:40:08 +0530

    update

---
+++ blog/src/components/Create.jsx
@@ -0,0 +1,30 @@
+import {useState} from "react";
+import { useNavigate } from "react-router-dom"
+
+export default function Create({setBlog}){
+    const [task,setTask]= useState("")
+    const [des,setDes]= useState("")
+    const navigate = useNavigate()
+
+    function HandleClick(){
+        if(task.trim() === "" || des.trim() === "")return;
+        const newTask={id:Date.now(),text:task,detail:des}
+        setBlog((prev)=> [...prev,newTask])
+        setTask("")
+        setDes("")
+        navigate("/View")
+        // console.log(`Title: ${newTask.text}, Details: ${newTask.detail}`)
+    }
+
+    return(
+        <>
+        <h2>Title</h2>
+        <input onChange={(e)=> setTask(e.target.value)}
+        value={task}/>
+        <h3>discribtion</h3>
+        <input onChange={(e)=> setDes(e.target.value)}
+        value={des}/>
+        <button onClick={HandleClick}>Add Blog</button>
+        </>
+    )
+}
