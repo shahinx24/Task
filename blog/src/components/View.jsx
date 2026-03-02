3315a945da493fc4c2d56bdf7cf9445f0ae45390
Shahin <shahinofficial911@gmail.com>
2026-03-02 10:38:26 +0530

    update

--- blog/src/components/View.jsx
+++ blog/src/components/View.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 export default function View({blog}){
     return(
         <>
+        <h1>Titels</h1>
         {blog.map((item)=>(
             <p key={item.id}>
                 <Link to={`/detail/${item.id}`}>
